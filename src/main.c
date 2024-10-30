e4da82cc5f8173573d108fc57b6786bebcf7de4c
Alexandre Messmer <alexandre.messmer@icloud.com>
2024-10-30 15:31:28 +0100

    Initial commit

--- src/main.c
+++ src/main.c
@@ -1,48 +1,17 @@
-/*
- * ============================================================================
- *
- *       Filename:  main.c
- *
- *    Description:  Main file of the project
- *
- *        Created:  03/24/2016 19:40:56
- *
- *         Author:  Gustavo Pantuza
- *   Organization:  Software Community
- *
- * ============================================================================
- */
-
-
 #include <stdio.h>
-#include <stdlib.h>
-
-#include "args.h"
-#include "colors.h"
 
 
-
-int
-main (int argc, char* argv[])
+int main(int argc, char const *argv[])
 {
+    int a[10] = {};
+    for (int i = 0; i < 10; i++) {
+        a[i] = i;
+    }
 
-    /* Read command line options */
-    options_t options;
-    options_parser(argc, argv, &options);
+    int j = 0;
+    printf("Please input the index of the element you want to access: ");
+    scanf("%d", &j);
 
-
-#ifdef DEBUG
-    fprintf(stdout, BLUE "Command line options:\n" NO_COLOR);
-    fprintf(stdout, BROWN "help: %d\n" NO_COLOR, options.help);
-    fprintf(stdout, BROWN "version: %d\n" NO_COLOR, options.version);
-    fprintf(stdout, BROWN "use colors: %d\n" NO_COLOR, options.use_colors);
-    fprintf(stdout, BROWN "filename: %s\n" NO_COLOR, options.file_name);
-#endif
-
-
-    /* Do your magic here :) */
-
-
-    return EXIT_SUCCESS;
+    printf("The %d-th element is %d\n", j, a[j]);
+    return 0;
 }
-
